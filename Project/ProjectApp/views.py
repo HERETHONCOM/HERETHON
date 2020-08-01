9d7c0d20070a56ee6fc5542c7e3cbfc8c1908b88
hyosunLee <leecrossun@gmail.com>
2020-08-02 00:43:51 +0900

    upload

--- Project/ProjectApp/views.py
+++ Project/ProjectApp/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import BlogPost
+from .models import Post
 
 # Create your views here.
 def main(requests):
     return render(requests,'main.html')
 
+def create(request):
+    if request.method == 'POST':
+        form = BlogPost(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=True)
+            return redirect('gallery')
+        return render(request, 'new.html', {'form':form})
+    else:
+        form = BlogPost()
+        return render(request, 'new.html', {'form':form})
