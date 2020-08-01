772beb69804743715f512729cfae9e0770e310b1
hyosunLee <leecrossun@gmail.com>
2020-08-02 03:06:31 +0900

    more

--- Project/ProjectApp/views.py
+++ Project/ProjectApp/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import BlogPost
 from .models import Post
+from django.core.paginator import Paginator
 
 # Create your views here.
-def main(requests):
-    return render(requests,'main.html')
+def main(request):
+    posts = Post.objects
+    post_list=Post.objects.all()
+    paginator = Paginator(post_list, 4) # 페이지 나누기 (객체 3개씩)
+    page = request.GET.get('page') # 요청한 페이지 값
+    pages = paginator.get_page(page) # 해당 페이지 가져오기
+    return render(request, 'main.html', {'posts':posts, 'pages':pages})
 
 def create(request):
     if request.method == 'POST':
